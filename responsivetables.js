013628d09b6b3f68c167dc62edcc781699b446f5
Max <mail@ne0h.de>
2016-05-10 16:37:07 +0200

    hotfix: separate contextes to use multiple times on same page

--- responsivetables.js
+++ responsivetables.js
@@ -1,10 +1,14 @@
 (function($) {
 	$.fn.responsiveTables = function() {
-		var result = "", tds = $("td"), trs = $("tr").length;
+		var result = "", tds = $("#" + $(this).attr("id") + " td"), trs = $("#" + $(this).attr("id") + " tr").length;
 		$.each(tds, function(i, v) {
-			result += "<div class='" + $(this).attr("class") + "' style='" + $(this).attr("style") + "; float: left;'>" + $(this).html().replace(/<td.*?(?=>)/g, "") + "</div>";
+			result += "<div"
+			if ($(this).attr("class")) result += " class='" + $(this).attr("class") + "'";
+			result += " style='";
+			if ($(this).attr("style")) result += $(this).attr("style");
+			result += "; float: left;'>" + $(this).html().replace(/<td.*?(?=>)/g, "") + "</div>";
 			if ((i + 1) % (tds.length / trs) == 0) {
-			result += "<div style='clear: left'></div>";
+				result += "<div style='clear: left'></div>";
 			}
 		});
 		this.html(result); return this;
