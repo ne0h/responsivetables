603adc5c0ae9e55fc8b871a0898a1eff8a98587f
Max <mail@ne0h.de>
2016-09-12 23:01:11 +0200

    Some important code improvements.

--- responsivetables.js
+++ responsivetables.js
@@ -4,9 +4,9 @@
 		$.each(tds, function(i, v) {
 			result += "<div"
 			if ($(this).attr("class")) result += " class='" + $(this).attr("class") + "'";
-			result += " style='";
-			if ($(this).attr("style")) result += $(this).attr("style");
-			result += "; float: left;'>" + $(this).html().replace(/<td.*?(?=>)/g, "") + "</div>";
+			result += " style=\"";
+			if ($(this).attr("style")) result += ($(this).attr("style").slice(-1) == ';') ? $(this).attr("style").substr(0, $(this).attr("style").length - 1) : result += $(this).attr("style");
+			result += "; float: left;\">" + $(this).html().replace(/<td.*?(?=>)/g, "") + "</div>";
 			if ((i + 1) % (tds.length / trs) == 0) {
 				result += "<div style='clear: left'></div>";
 			}
